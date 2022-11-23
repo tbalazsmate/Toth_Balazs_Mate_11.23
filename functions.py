55b3b6130e679d19ee12899e4c9dcceeacacabb4
09.D_Tóth Balázs Máté <toth.balazs.mate@students.jedlik.eu>
2022-11-23 09:26:20 +0100

    Fuctions menü
    Elkészült a menü

--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
-from data import datum,homerseklet
+from data import datumok,homersekletek
+from os import system
+
+def menu():
+    system('cls')
+    print('----------MENÜ----------')
+    print('0 - Kilépés')
+    print('1 - Adat felvétele')
+    print('2 - Adat törlése')
+    print('3 - Átlag hőmérséklet')
+    print('4 - Legmelegebb nap')
+    print('5 - Leghidegebb nap')
+    
